cd682bb3fea7c4427716eff2788cb2280958c934
heisraymond <imraymond07@gmail.com>
2025-07-29 20:33:13 +0300

    Succesfully created the form functionality and added a static directory

--- crm/views.py
+++ crm/views.py
@@ -5,11 +5,13 @@ from . forms import DailyNoteForm
 
 def home(request):
     if request.method == 'POST':
+        print(request)
+        # Assign the form to a var ' form'
         form = DailyNoteForm(request.POST)
         if form.is_valid():
             form.save()
             
-            return redirect('home')
+            return redirect(home)
     else:
         form = DailyNoteForm()
     
--- project/urls.py
+++ project/urls.py
@@ -20,5 +20,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    # We are including all urls from my 
+    # app in our project folder
     path("", include('crm.urls')),
 ]
